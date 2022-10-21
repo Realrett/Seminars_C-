c5496fbb807b44a5b9dcf89b0f6d15695d14bbe7
Realrett <gosha-student@mail.ru>
2022-10-21 18:46:45 +0700

    Решение задачи 2

--- Exersize_1/Program.cs
+++ Exersize_1/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Сравниваем 2 числа
+Console.WriteLine("Введите первое число");
+string numberA = Console.ReadLine();
+int number1 = int.Parse(numberA);
+
+Console.WriteLine("Введите второе число");
+string numberB = Console.ReadLine();
+int number2 = int.Parse(numberB);
+
+if (number1 > number2)
+{
+    Console.WriteLine("Первое число большее из двух");
+}
+else
+{
+    Console.WriteLine("Второе число большее из двух");
+}
