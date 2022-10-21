e0bf76537a16ca0381671d551c4c5fa074a695af
Realrett <gosha-student@mail.ru>
2022-10-21 21:20:11 +0700

    Чётные значения

---
+++ Exersize_4/Program.cs
@@ -0,0 +1,28 @@
+﻿// вводим число N и показываем все четные значения от 1 до N
+Console.WriteLine("Введите число N:");
+string numberA = Console.ReadLine();
+int number1 = int.Parse(numberA);
+int number2 = 1;
+if(number1 > 0)
+{
+for(int i = number1-1; i > number2; i--)
+{
+    if(i % 2 ==0)
+    {
+    Console.WriteLine($"Чётные числа, находящиеся в промежутке {i}");
+    }
+}
+}
+else
+{
+  for(int i = number1+1; i < number2; i++)
+{
+    if(i % 2 ==0)
+    {
+    Console.WriteLine($"Чётные числа, находящиеся в промежутке {i}");
+    }
+}  
+}  
+
+
+
