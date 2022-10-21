0bfb08eae935993f9427ce2e85eb54ed913e5413
Realrett <gosha-student@mail.ru>
2022-10-21 19:25:45 +0700

    Решил задачу 4

---
+++ Exersize_2/Program.cs
@@ -0,0 +1,40 @@
+﻿// Сравниваем 3 числа
+Console.WriteLine("Сравним 3 числа и найдем большее. Введите первое число");
+string numberA = Console.ReadLine();
+int number1 = int.Parse(numberA);
+
+Console.WriteLine("Введите второе число");
+string numberB = Console.ReadLine();
+int number2 = int.Parse(numberB);
+
+Console.WriteLine("Введите третье число");
+string numberС = Console.ReadLine();
+int number3 = int.Parse(numberС);
+
+int max = number1;
+
+if (max >= number2)
+{
+if (max >= number3)
+{
+ Console.WriteLine($"Наибольшее число {max}");
+}
+else
+{
+    int max3 = number3;
+    Console.WriteLine($"Наибольшее число {max3}");
+}
+}
+else
+{
+    int max2 = number2;
+    if (max2 >= number3)
+{
+ Console.WriteLine($"Наибольшее число {max2}");
+}
+else
+{
+int max4 = number3;
+ Console.WriteLine($"Наибольшее число {max4}");
+}
+}
