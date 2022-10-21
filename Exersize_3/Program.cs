ee6486afab3c288d5180cfc5f0747195554c0091
Realrett <gosha-student@mail.ru>
2022-10-21 19:46:11 +0700

    Задача 6

---
+++ Exersize_3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Проверить делится ли число без остатка на 2
+Console.WriteLine("Проверим является ли число чётным? Введите число:");
+string numberA = Console.ReadLine();
+int number1 = int.Parse(numberA);
+if (number1 % 2 ==0)
+{
+    Console.WriteLine("Число чётное");
+}
+else
+{
+  Console.WriteLine("Число нечётное");  
+}
